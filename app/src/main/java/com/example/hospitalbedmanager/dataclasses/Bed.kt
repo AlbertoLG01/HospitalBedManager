f7a096c381c6446efccf80785aca4d5d84e55e33
Abeto <abetol2001@gmail.com>
2024-06-20 20:03:27 +0200

    20/06 NurseFragment acabado, falta seguir con el porter y las camas ocupadas. Por último el de doctor

---
+++ app/src/main/java/com/example/hospitalbedmanager/dataclasses/Bed.kt
@@ -0,0 +1,11 @@
+package com.example.hospitalbedmanager.dataclasses
+
+import com.google.firebase.Timestamp
+
+data class Bed(
+    val number: Int = -1,
+    val patientAssociated: String = "",
+    val consultationAssociated: Int = -1,
+    val isOccupied: Boolean = false,
+    val assignmentDate: Timestamp = Timestamp.now()
+)
--- app/src/main/java/com/example/hospitalbedmanager/ui/nurse/NurseFragment.kt
+++ app/src/main/java/com/example/hospitalbedmanager/ui/nurse/NurseFragment.kt
@@ -1,42 +1,185 @@
 package com.example.hospitalbedmanager.ui.nurse
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hospitalbedmanager.R
 import com.example.hospitalbedmanager.databinding.FragmentNurseBinding
+import com.example.hospitalbedmanager.dataclasses.Bed
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class NurseFragment : Fragment() {
 
     private var _binding: FragmentNurseBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var bedAdapter: BedAdapter
+    private val bedList = mutableListOf<Bed>()
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(NurseViewModel::class.java)
-
         _binding = FragmentNurseBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        setupRecyclerView()
+        fetchBeds()
+
         return root
     }
+
+    private fun setupRecyclerView() {
+        bedAdapter = BedAdapter(requireContext(), bedList) { bed ->
+            showAssignDialog(bed)
+        }
+        binding.recyclerViewCamas.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = bedAdapter
+        }
+    }
+
+    private fun fetchBeds() {
+        db.collection("beds")
+            .whereEqualTo("isOccupied", false)
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val bed = document.toObject(Bed::class.java)
+                    bedList.add(bed)
+                }
+                bedAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(context, "Error al obtener camas: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun showAssignDialog(bed: Bed) {
+        val builder = AlertDialog.Builder(requireContext())
+        val dialogView = layoutInflater.inflate(R.layout.dialog_assign_bed, null)
+        val editTextPatientId = dialogView.findViewById<EditText>(R.id.editTextPatientId)
+        val editTextConsultationNumber = dialogView.findViewById<EditText>(R.id.editTextConsultationNumber)
+
+        builder.setView(dialogView)
+            .setTitle("Asignar Cama")
+            .setPositiveButton("Asignar") { _, _ ->
+                val patientName = editTextPatientId.text.toString()
+                val consultationNumber = editTextConsultationNumber.text.toString().toInt()
+
+                if (patientName.isNotEmpty() && consultationNumber != -1) {
+                    assignBed(bed.number, patientName, consultationNumber)
+                } else {
+                    Toast.makeText(context, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .setNegativeButton("Cancelar", null)
+            .create()
+            .show()
+    }
+
+    private fun assignBed(bedNumber: Int, patientName: String, consultationNumber: Int) {
+        db.collection("beds")
+            .whereEqualTo("number", bedNumber)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (documents.isEmpty) {
+                    Toast.makeText(context, "Cama no encontrada", Toast.LENGTH_SHORT).show()
+                    return@addOnSuccessListener
+                }
+
+                // Asumiendo que solo hay un documento con este número de cama
+                val bedDoc = documents.documents[0]
+                val bedRef = bedDoc.reference
+
+                bedRef.update(
+                    mapOf(
+                        "patientAssociated" to patientName,
+                        "consultationAssociated" to consultationNumber,
+                        "isOccupied" to true,
+                        "assignmentDate" to Timestamp.now()
+                    )
+                ).addOnSuccessListener {
+                    Toast.makeText(context, "Cama asignada exitosamente", Toast.LENGTH_SHORT).show()
+                    // Elimina la cama de la lista local
+                    bedList.removeIf { it.number == bedNumber }
+                    bedAdapter.notifyDataSetChanged()
+                }.addOnFailureListener { exception ->
+                    Toast.makeText(context, "Error al asignar cama: ${exception.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(context, "Error al buscar cama: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    class BedAdapter(
+        private val context: Context,
+        private val bedList: List<Bed>,
+        private val onAssignClickListener: (Bed) -> Unit
+    ) : RecyclerView.Adapter<BedAdapter.BedViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BedViewHolder {
+            val view = LayoutInflater.from(context).inflate(R.layout.item_free_bed, parent, false)
+            return BedViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: BedViewHolder, position: Int) {
+            val bed = bedList[position]
+            holder.bedNumber.text = "Cama número ${bed.number}"
+            // Formatear la fecha de asignación
+            val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
+            val assignmentDate = bed.assignmentDate.toDate()
+            val formattedDate = sdf.format(assignmentDate)
+            holder.lastAssignment.text = "Última Asignación: $formattedDate"
+
+            if(bed.consultationAssociated != -1) {
+                holder.consultationNumber.text =
+                    "Número de última consulta asociada: ${bed.consultationAssociated}"
+            }
+            else{
+                holder.consultationNumber.text =
+                    "Número de última consulta asociada: No hay registro"
+            }
+
+            holder.assignButton.setOnClickListener {
+                onAssignClickListener(bed)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return bedList.size
+        }
+
+        class BedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val bedNumber: TextView = itemView.findViewById(R.id.text_bed_number)
+            val lastAssignment: TextView = itemView.findViewById(R.id.text_last_assignment)
+            val consultationNumber: TextView = itemView.findViewById(R.id.text_consultation_number)
+            val assignButton: Button = itemView.findViewById(R.id.button_assign)
+        }
+    }
+
 }
+
